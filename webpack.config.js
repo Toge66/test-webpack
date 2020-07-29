8dc4507f86deb772540fe8132f8f95fb7e15ceca
taochang <toge0223@gmail.com>
2020-07-29 17:40:30 +0800

    multi entry

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    app: './src/app.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
